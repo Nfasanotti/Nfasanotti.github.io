afb488d2edc8059576d810efefda5b807c982dc0
Natasha Fasanotti <fasanotti.n@gmail.com>
2019-10-22 16:50:09 +0100

    Java Script

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ var UserName=prompt('Please enter your name below', "Stranger");
 if (UserName!=null){
 $("#UserName").innerHTML="Hi " +UserName+ "! Press the button to try the quiz";
 }   
+
+$(document).ready(function(){
+    $('#NameState').hide();
 $("#Button1").click(ButtonClicked(){
     $('#NameState').show();
-$(document).ready(function(){
 })
 function ChooseColour(){
     let Red=Math.floor(Math.random()*(256-0+1));
