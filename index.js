7cca06f8de7518956af983263bf8fdc0636833f1
Natasha Fasanotti <fasanotti.n@gmail.com>
2019-10-22 16:21:12 +0100

    Java Script

---
+++ index.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+var UserName=prompt('Please enter your name below', "Stranger");
+if (UserName!=null){
+$("UserName").innerHTML="Hi " +UserName+ "! Press the button to try the quiz";
+}   
+function ButtonClicked(){
+    $("Button").innerHTML="It's time to test how well you know your colours";
+}
+function ChooseColour(){
+    let Red=Math.floor(Math.random()*(256-0+1));
+    let Green=Math.floor(Math.random()*(256-0+1));
+    let Blue=Math.floor(Math.random()*(256-0+1));
+    return(rgb(Red,Green,Blue))
+}
+let CorrectColour=ChooseColour()
+$("Original").style.backgroundColor="red"
+})
