177e14530fb86207a83ec5e2189e9a794aaa5cb3
Natasha Fasanotti <fasanotti.n@gmail.com>
2019-10-23 14:54:07 +0100

    JQuery

--- index.js
+++ index.js
@@ -14,9 +14,9 @@ $(document).ready(function(){
 
 $("#Start").click(function(){
     $("#Original").show()
- /*   $.get("https://opentdb.com/api.php?amount=10&category=11&difficulty=medium", function(data){
+    $.get("https://opentdb.com/api.php?amount=10&category=11&difficulty=medium", function(data){
       $("#Quiz").text(data)
-*/    })
+    })
 
 
 
@@ -34,7 +34,7 @@ $("#Start").click(function(){
         // $("span").text(quiz)
     // }
 //})
-//})
+})
 
 
 
