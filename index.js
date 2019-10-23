6cfaa78699a3a4ecf9230673c5432d1919713e48
Natasha Fasanotti <fasanotti.n@gmail.com>
2019-10-23 10:47:37 +0100

    Java Script

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 
-var UserName=$(":input");
+$('#NameEntered').click(function({
+    var Name=$("#UserName").val();
+    if(Name!=null){
+        alert("Hi " +UserName+ "! Press the button to try the quiz");
+    }
+}))
+
+/*var UserName=$(":input");
 $('#HiMessage').text("Hi "+UserName+" ! Press the button to try the quiz")
 
-/*var UserName=prompt('Please enter your name below', "Stranger");
+var UserName=prompt('Please enter your name below', "Stranger");
 if (UserName!=null){
 $("#UserName").innerHTML="Hi " +UserName+ "! Press the button to try the quiz";
 }   
