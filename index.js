c92e5862b3fd7ca446153fc7f1cc1df3e83acbcc
Natasha Fasanotti <fasanotti.n@gmail.com>
2019-10-23 14:01:06 +0100

    JQuery

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
 
 
 $(document).ready(function(){
+    $("#Original").hide();
     $('#NameEntered').click(function(){
         var Name=$('#UserName').val();
         console.log($("#UserName"))
         if(Name!=null){
-            $("span").text("Hi " +Name+ "! Press the button to try the quiz");
+            alert("Hi " +Name+ "! Press the button to try the quiz");
         }
     })
 
 
 $("#Start").click(function(){
-    $("#Original").css("background-color",rgb(255,0,0))
+    $("#Original").show()
+    $.getJSON("https://opentdb.com/api.php?amount=10&category=11&difficulty=medium"), function(json){
+        let quiz=("json")
 
 
 
