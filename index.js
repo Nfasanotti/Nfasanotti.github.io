5a31dd4f37b1a12a3d92b382d5ce78c5eee5db20
Natasha Fasanotti <fasanotti.n@gmail.com>
2019-10-23 12:40:38 +0100

    JQuery

--- index.js
+++ index.js
@@ -9,23 +9,13 @@ $(document).ready(function(){
         }
     })
 
-function ChooseColour(){
-    let Red=Math.floor(Math.random()*(256-0+1));
-    let Green=Math.floor(Math.random()*(256-0+1));
-    let Blue=Math.floor(Math.random()*(256-0+1));
-    return(rgb(Red,Green,Blue))
-}
 
 $("#Start").click(function(){
-    $("#Original").css("background-color","rgb(ChooseColour()");
+    $.getJSON("https://opentdb.com/api.php?amount=10&category=11&difficulty=medium"), function(json){
+        console.log(json);
+    }
+})
 })
 
-$.getJSON("https://opentdb.com/api.php?amount=10&category=11&difficulty=medium"), function(json){
-    console.log(json);
-}
-
-})
-
-})
 
 
