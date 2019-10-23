e4efb0b0a739a38a7f1e900de395b74f37053580
Natasha Fasanotti <fasanotti.n@gmail.com>
2019-10-23 15:40:05 +0100

    JQuery

--- index.js
+++ index.js
@@ -15,8 +15,10 @@ $(document).ready(function(){
 $("#Start").click(function(){
     $("#Original").show()
     var api_Url="https://opentdb.com/api.php?amount=10&category=11&difficulty=medium"
+    alert(api_Url)
     $.ajax({
         url: api_Url,
+        data: data,
         dataType:'json',
         success: function(result){
             console.log(esult)
