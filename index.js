b7bf0b803a7f13105dc2c989cc9cf5515cd76128
Natasha Fasanotti <fasanotti.n@gmail.com>
2019-10-25 11:28:35 +0100

    Answers

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ $(document).ready(function(){
     $("#ButtonB").hide();
     $("#ButtonC").hide();
     $("#ButtonD").hide();
+    $("#redo").hide()
     $('#NameEntered').click(function(){
         var Name=$('#UserName').val();
         console.log($("#UserName"))
@@ -19,91 +20,86 @@ $(document).ready(function(){
         }
     })
 function quiz(){
-QuestionNum=0
-Score=0
-$.get(src, function(data){
-$("#Start").click(function(){
-    function reset(){
-        $("#ButtonA").css({"background-color":"lightgray"})
-        $("#ButtonB").css({"background-color":"lightgray"})
-        $("#ButtonC").css({"background-color":"lightgray"})
-        $("#ButtonD").css({"background-color":"lightgray"})
-    }
-    reset()
-    function undo(){
-        $("#ButtonA").unbind()
-        $("#ButtonB").unbind()
-        $("#ButtonC").unbind()
-        $("#ButtonD").unbind()
-
-    }
-    if (QuestionNum<10) {
-            let CorrectAns=(data.results[QuestionNum].correct_answer);
-            let IncorrectAns=(data.results[QuestionNum].incorrect_answers);
-         $("#Question").html(data.results[QuestionNum].question);
-         let Ans=IncorrectAns;
-         Ans.push(CorrectAns);
-         console.log(Ans);
-         Ans.sort()
-        
-
-         $("#display_ans").html (Ans.join(",  "));
-         $("#Question").show();
-         $("label").show();
-         $("button").show();
-         $("#ButtonA").click(function(){
-            reset()
-             if (Ans[0]==CorrectAns){
-                $("#ButtonA").css({"background-color":"#4aff03"}) 
-                Score+=1
-             }else{
-                 $("#ButtonA").css({"background-color":"#ff0303"})
+    QuestionNum=0
+    Score=0
+    $.get(src, function(data){
+    $("#Start").click(function(){
+     function reset(){
+          $("#ButtonA").css({"background-color":"lightgray"})
+            $("#ButtonB").css({"background-color":"lightgray"})
+            $("#ButtonC").css({"background-color":"lightgray"})
+         $("#ButtonD").css({"background-color":"lightgray"})
+     }
+     reset()
+     function undo(){
+         $("#ButtonA").unbind()
+            $("#ButtonB").unbind()
+            $("#ButtonC").unbind()
+         $("#ButtonD").unbind()
+     }
+     if (QuestionNum<10) {
+             let CorrectAns=(data.results[QuestionNum].correct_answer);
+             let IncorrectAns=(data.results[QuestionNum].incorrect_answers);
+             $("#Question").html(data.results[QuestionNum].question);
+             let Ans=IncorrectAns;
+             Ans.push(CorrectAns);
+             console.log(Ans);
+            Ans.sort()
+             $("#display_ans").html (Ans.join(",  "));
+             $("#Question").show();
+             $("label").show();
+             $("button").show();
+             $("#ButtonA").click(function(){
+                 reset()
+                if (Ans[0]==CorrectAns){
+                     $("#ButtonA").css({"background-color":"#4aff03"}) 
+                  Score+=1
+                 }else{
+                     $("#ButtonA").css({"background-color":"#ff0303"})
                 }
                 undo()
+            })
+             $("#ButtonB").click(function(){
+                 reset()
+                 if (Ans[1]==CorrectAns){
+                     $("#ButtonB").css({"background-color":"#4aff03"}) 
+                     Score+=1
+                  }else{
+                     $("#ButtonB").css({"background-color":"#ff0303"})
+                 }
+             undo()
          })
-         $("#ButtonB").click(function(){
-            reset()
-            if (Ans[1]==CorrectAns){
-               $("#ButtonB").css({"background-color":"#4aff03"}) 
-               Score+=1
-            }else{
-                $("#ButtonB").css({"background-color":"#ff0303"})
-            }
-            undo()
-        })
-        $("#ButtonC").click(function(){
-            reset()
-            if (Ans[2]==CorrectAns){
-               $("#ButtonC").css({"background-color":"#4aff03"}) 
-               Score+=1
-            }else{
-                $("#ButtonC").css({"background-color":"#ff0303"})
-            }
-        undo()
-        })
-        $("#ButtonD").click(function(){
-            reset()
-            if (Ans[3]==CorrectAns){
-               $("#ButtonD").css({"background-color":"#4aff03"}) 
-               Score+=1
-            }else{
-                $("#ButtonD").css({"background-color":"#ff0303"})
-            }
-            undo()
-        })        
-         QuestionNum+=1 
+             $("#ButtonC").click(function(){
+                reset()
+                if (Ans[2]==CorrectAns){
+                  $("#ButtonC").css({"background-color":"#4aff03"}) 
+                 Score+=1
+              }else{
+                  $("#ButtonC").css({"background-color":"#ff0303"})
+             }
+             undo()
+            })
+          $("#ButtonD").click(function(){
+              reset()
+              if (Ans[3]==CorrectAns){
+                  $("#ButtonD").css({"background-color":"#4aff03"}) 
+                  Score+=1
+              }else{
+                 $("#ButtonD").css({"background-color":"#ff0303"})
+             }
+             undo()
+         })        
+          QuestionNum+=1 
+     }
+     else{
+         $("#redo").show()
+          $("#final_score").text("Your final score is: "+Score)
+        }
+         })
+    })
     }
-    else{
-        $("#redo").show()
-        $("#final_score").text("Your final score is: "+Score)
-    }
-
-
-        })
-})
-}
-quiz()
-})
+    quiz()
+    })
 
 
 
